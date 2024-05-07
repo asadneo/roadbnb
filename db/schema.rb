8d71a6fd2f9d46ea521f36be4b65346e364b3c08
Asad Mirza <asadneo@yahoo.com>
2024-05-07 15:07:35 +0100

    added password column in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_07_113839) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_07_135916) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_07_113839) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
   add_foreign_key "bookings", "cars"
