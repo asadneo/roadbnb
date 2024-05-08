1af67ab5b119adecabdd4e0eb7ed19c97a951ccd
Giacomo fargion <giacomo.fargion@yahoo.com>
2024-05-08 16:37:58 +0100

    incorporated booking creation user journey

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,24 @@
 class BookingsController < ApplicationController
+  def new
+    @car = Car.find(params[:car_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @car = Car.find(params[:car_id])
+    @booking = Booking.new(booking_params)
+    @booking.user_id = current_user.id
+    @booking.car = @car
+    if @booking.save
+      redirect_to car_path(@car)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date)
+  end
 end
